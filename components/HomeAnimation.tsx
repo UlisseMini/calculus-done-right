6f5e75cf28b835b4cbc29519e14fb3f78808a33e
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-02 14:25:49 -0500

    Fix aspect ratio on mobile

--- components/HomeAnimation.tsx
+++ components/HomeAnimation.tsx
@@ -1,8 +1,11 @@
+import { useClientDimensions } from "lib/utils";
 import { Mafs, FunctionGraph, useStopwatch, Theme } from "mafs";
 import { useEffect } from "react";
 
 const cos = Math.cos;
 const sin = Math.sin;
+
+// TODO: gif or video this for slow devices (svg isn't that performant)
 
 export default function HomeAnimation() {
   const { time, start } = useStopwatch();
@@ -11,10 +14,14 @@ export default function HomeAnimation() {
   const k = cos(time * 0.3); // periodic coefficient used in the animation
   const tau = 2 * Math.PI;
 
+  const { clientWidth } = useClientDimensions();
+  // subtract 8 to deal with padding issue (its a hack I know)
+  const size = Math.min(500, clientWidth - 8);
+
   // <FunctionGraph.OfX y={f} color={Theme.blue} />
   return (
     <>
-      <Mafs pan={false}>
+      <Mafs pan={false} width={size} height={size}>
         <FunctionGraph.Parametric
           xy={(t) => [
             (t * cos(k * (t + 0.1 * cos(t)) * tau)) / 4,
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const assert = (checkFn: () => boolean, msg?: string) => {
   if (!checkFn()) {
     const checkString = checkFn.toString().replace(/\(\) ?=> ?/, "");
@@ -7,3 +9,20 @@ export const assert = (checkFn: () => boolean, msg?: string) => {
 
 export const plural = (num: number, word: string) =>
   `${num} ${word}${num !== 1 ? "s" : ""}`;
+
+export const useClientDimensions = () => {
+  const [dims, setDims] = useState({
+    // default to mobile, this is stupid
+    clientHeight: 667,
+    clientWidth: 375,
+  });
+
+  useEffect(() => {
+    setDims({
+      clientHeight: document.documentElement.clientHeight,
+      clientWidth: document.documentElement.clientWidth,
+    });
+  }, []);
+
+  return dims;
+};
